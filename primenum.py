762465129e33ecb7c3b3709a2ca63065b1ac8f92
Jason Xu <15933353154@sina.cn>
2017-01-17 13:03:00 +0800

    fix bug

--- primenum.py
+++ primenum.py
@@ -2,22 +2,23 @@
 
 def primenum(n):
 	X = True
-	list = [2, 3, 5, 7]
-	import math
-	s = math.floor(math.sqrt(n))
-	if s % 2 == 0:
-		s -= 1
-	if n not in list and s > list[-1]:
-		for i in list:
-			if n % i == 0:
-				X = False
-				break
-		if X:
-			while s > list[-1]:
-				if n % s == 0:
+	list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 77, 79, 83, 89, 91, 97]
+	if n not in list:
+		if n < list[-1]:
+			X = False
+		if n > list[-1]:
+			for i in list:
+				if n % i == 0:
 					X = False
 					break
-				s -= 2
+			if X:
+				import math
+				s = math.floor(math.sqrt(n))
+				while s > list[-1]:
+					if n % s == 0:
+						X = False
+						break
+					s -= 2
 	return X
 N = int(input("Input:"))
 if primenum(N):
